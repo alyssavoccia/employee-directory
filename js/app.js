db0be71f4d03eb683f859245ccd742055752400e
Alyssa Voccia <avoccia@hotmail.com>
2021-05-30 13:15:36 -0400

    Revert "Update app.js"
    This reverts commit d6ce189982755c3923efdba7101d09c760d5ddd9.

--- js/app.js
+++ js/app.js
@@ -73,7 +73,7 @@ function abbrState(input, to){
 
 // Function to capitalize the first letter of every word
 function titleCase(str) {
-  var splitStr = str.toString().toLowerCase().split(' ');
+  var splitStr = str.toLowerCase().split(' ');
   for (var i = 0; i < splitStr.length; i++) {
     splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
   }
